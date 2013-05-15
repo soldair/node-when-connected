85e15edf1c9ae644bde6aa0a61c88f23fc19440e
Ryan Day <soldair@gmail.com>
2013-05-15 09:23:54 -0700

    fixing disconnect error and dequeuing timedout callbacks

--- index.js
+++ index.js
@@ -42,11 +42,12 @@ module.exports = function(fn,opts){
 
         if(!connected) {
           var index = binarysearch(q,id);
-          if(index > 0) q.splice(index,1);
+          if(index > -1) q.splice(index,1);
         }
  
       },opts.timeout);
     }
+    // binary search defaults to looking for a v key in objects as the sorted value.
     var data = {a:args,t:timeout,s:s,v:i++};
     if(connected) run(fn,data);
     else q.push(data)
@@ -83,8 +84,15 @@ function run(fn,args) {
   var res = fn.apply(null,args);
   if(stream) {
     clearTimeout(timer);
-    if(res) res.pipe(stream);
-    else {
+    if(res) {
+
+      res.on('error',function(e){
+        stream.emit('error',e);
+      });
+
+      res.pipe(stream);
+
+    } else {
       var e = new Error('E_NOTSTREAM function did not return a stream but stream was expected. '+fn.toString());
       e.code = 'E_NOTSTREAM';
       stream.emit('error',e);
